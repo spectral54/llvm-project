9bf1922369c55d6f228a39a73cb24465426c4449
Kyle Lippincott <spectral@google.com>
2024-08-23 13:02:45 -0700

    Add regexec test

---
+++ compiler-rt/test/asan/TestCases/regexec-1.c
@@ -0,0 +1,107 @@
+// RUN: %clang_asan -O2 %s -o %t
+// RUN: %run %t test1 2>&1 | FileCheck %s --check-prefix=TEST1
+// RUN: %run %t test2 2>&1 | FileCheck %s --check-prefix=TEST2
+// FIXME: this should not fail.
+// RUN: not %run %t test3 2>&1 | FileCheck %s --check-prefix=TEST3
+
+#include <regex.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sanitizer/asan_interface.h>
+
+void regexec_or_exit(const regex_t *preg, const char *str, size_t nmatch,
+                     regmatch_t pmatch[], int eflags) {
+  int err = regexec(preg, str, nmatch, pmatch, eflags);
+  if (err != 0) {
+    printf("Error in regexec: %d\n", err);
+    exit(1);
+  }
+}
+
+void test1(regex_t *re) {
+  regmatch_t match[1];
+  char str[] = "ABCDEF";
+
+  // Check that regexec works fine under Asan.
+  regexec_or_exit(re, str, /*nmatch=*/1, /*pmatch=*/match, /*flags=*/0);
+  printf("MATCHED: %.*s\n", match[0].rm_eo - match[0].rm_so,
+         str + match[0].rm_so);
+}
+
+void test2(regex_t *re) {
+#ifndef REG_STARTEND
+  // Print the expected line if REG_STARTEND is unavailable
+  printf("MATCHED: BC\n");
+  return;
+#endif
+  regmatch_t match[1];
+  char str[] = "ABCDEF";
+
+  // Test basic functionality of REG_STARTEND.
+  match[0].rm_so = 1;
+  match[0].rm_eo = 3;
+  regexec_or_exit(re, str, /*nmatch=*/1, /*pmatch=*/match,
+                  /*flags=*/REG_STARTEND);
+  printf("MATCHED: %.*s\n", match[0].rm_eo - match[0].rm_so,
+         str + match[0].rm_so);
+}
+
+void test3(regex_t *re) {
+#ifndef REG_STARTEND
+  // Print the expected line if REG_STARTEND is unavailable
+  printf("MATCHED: DE\n");
+  return;
+#endif
+  regmatch_t match[1];
+
+  // Check that asan itself doesn't do the wrong thing and interact with/mask
+  // memory outside of the specified region.
+  char buf[300] = {'X'};
+  strcpy(&buf[100], "ABCDEF");
+  __asan_poison_memory_region(buf, sizeof(buf));
+  __asan_unpoison_memory_region(&buf[103], 2);
+
+  match[0].rm_so = 103;
+  match[0].rm_eo = 105;
+  regexec_or_exit(re, buf, /*nmatch=*/1, /*pmatch=*/match,
+                  /*flags=*/REG_STARTEND);
+  printf("MATCHED: %.*s\n", match[0].rm_eo - match[0].rm_so,
+         buf + match[0].rm_so);
+}
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    printf("Invalid number of arguments\n");
+    exit(1);
+  }
+
+  regex_t re;
+  int err = regcomp(&re, ".*", 0);
+  if (err != 0) {
+    printf("Error in regcomp: %d\n", err);
+    exit(1);
+  }
+
+  int ret = 0;
+
+  if (!strcmp(argv[1], "test1"))
+    test1(&re);
+  // TEST1: MATCHED: ABCDEF
+  else if (!strcmp(argv[1], "test2"))
+    test2(&re);
+  // TEST2: MATCHED: BC
+  else if (!strcmp(argv[1], "test3"))
+    test3(&re);
+  // FIXME: This should be 'MATCHED: DE', not an asan failure.
+  // TEST3: use-after-poison
+  else {
+    printf("Error: %s not a valid test", argv[1]);
+    ret = 1;
+  }
+
+  regfree(&re);
+
+  return ret;
+}
